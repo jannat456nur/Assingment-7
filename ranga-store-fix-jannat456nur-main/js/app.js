c77566940cac07f68560cd76bf3d3cbeff318c78
jannat456nur <jannatjenny22@gmail.com>
2021-09-12 18:08:29 -0700

    add css

--- ranga-store-fix-jannat456nur-main/js/app.js
+++ ranga-store-fix-jannat456nur-main/js/app.js
@@ -19,11 +19,11 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <p>Category: ${product.rating.rate}</p>
-      <p>Category: ${product.rating.count}</p>
+      <p>rate: ${product.rating.rate}</p>
+      <p>count: ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn jannat-btn-success">add to cart</button>
+      <button id="details-btn" class="btn jannat-btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
